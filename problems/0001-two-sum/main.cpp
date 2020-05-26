dfa368cb8888dd97cbe9a0b1ab8377d77da8a701
Shohei YOSHIDA <syohex@gmail.com>
2020-05-27 00:49:34 +0900

    Fix test case

--- problems/0001-two-sum/main.cpp
+++ problems/0001-two-sum/main.cpp
@@ -3,7 +3,7 @@
 
 std::vector<int> twoSum(const std::vector<int> &nums, int target) {
     for (size_t i = 0; i < nums.size() - 1; ++i) {
-        for (size_t j = 1; j < nums.size(); ++j) {
+        for (size_t j = i + 1; j < nums.size(); ++j) {
             if (nums[i] + nums[j] == target) {
                 return std::vector<int>{static_cast<int>(i), static_cast<int>(j)};
             }
@@ -25,5 +25,8 @@ int main() {
 
     ret = twoSum(std::vector<int>{1, 2, 3}, 4);
     assert(ret[0] == 0 && ret[1] == 2);
+
+    ret = twoSum(std::vector<int>{2, 5, 5, 11}, 10);
+    assert(ret[0] == 1 && ret[1] == 2);
     return 0;
 }
