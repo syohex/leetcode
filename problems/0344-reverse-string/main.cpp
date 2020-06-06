5366ea6a7cfbfe70d277c529f49cd66784cdc297
Shohei YOSHIDA <syohex@gmail.com>
2020-06-06 09:21:01 +0900

    Add problem 0344

---
+++ problems/0344-reverse-string/main.cpp
@@ -0,0 +1,30 @@
+#include <cassert>
+#include <algorithm>
+#include <vector>
+
+void reverseString(std::vector<char>& s) {
+    size_t size = s.size();
+    size_t limit = size / 2;
+    for (size_t i = 0; i < limit; ++i) {
+        std::swap(s[i], s[size - 1 - i]);
+    }
+}
+
+int main() {
+    std::vector<char> input{'H', 'e', 'l', 'l', 'o'};
+    reverseString(input);
+    assert((input == std::vector<char>{'o', 'l', 'l', 'e', 'H'}));
+    
+    input = std::vector<char>{'H', 'a', 'n', 'n', 'a', 'h'};
+    reverseString(input);
+    assert((input == std::vector<char>{'h', 'a', 'n', 'n', 'a', 'H'}));
+
+    input = std::vector<char>{'H'};
+    reverseString(input);
+    assert((input == std::vector<char>{'H'}));
+
+    input = std::vector<char>{'a', 'b'};
+    reverseString(input);
+    assert((input == std::vector<char>{'b', 'a'}));
+    return 0;
+}
