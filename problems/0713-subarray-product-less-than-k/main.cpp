34c5afce4ee9583c140353771c7ca6fe27ed8910
Shohei YOSHIDA <syohex@gmail.com>
2020-06-17 08:24:58 +0900

    Reduce calculations but still slow

--- problems/0713-subarray-product-less-than-k/main.cpp
+++ problems/0713-subarray-product-less-than-k/main.cpp
@@ -5,16 +5,24 @@
 
 int numSubarrayProductLessThanK(const std::vector<int> &nums, int k) {
     std::function<int(int, size_t)> f;
-    f = [&f, &nums, &k](int product, size_t pos) -> int {
-        if (pos >= nums.size()) {
-            return 0;
-        }
-
+    int val;
+    f = [&f, &nums, &k, &val](int product, size_t pos) -> int {
         int ret = 0;
         int p = product * nums[pos];
         if (p < k) {
+            if (pos == nums.size() - 1) {
+                val = p;
+            }
             ++ret;
-            ret += f(p, pos + 1);
+
+            for (size_t i = pos + 1; i < nums.size(); ++i) {
+                if (nums[i] == 1) {
+                    ++ret;
+                } else {
+                    ret += f(p, i);
+                    break;
+                }
+            }
         }
 
         return ret;
@@ -22,7 +30,14 @@ int numSubarrayProductLessThanK(const std::vector<int> &nums, int k) {
 
     int ret = 0;
     for (size_t i = 0; i < nums.size(); ++i) {
+        val = 0;
         ret += f(1, i);
+        if (val != 0 && val < k) {
+            for (size_t j = i + 1; j < nums.size(); ++j) {
+                ret += nums.size() - j;
+            }
+            break;
+        }
     }
     return ret;
 }
@@ -31,6 +46,10 @@ int main() {
     {
         std::vector<int> input{10, 5, 2, 6};
         assert(numSubarrayProductLessThanK(input, 100) == 8);
+    }
+    {
+        std::vector<int> input{1, 1, 1};
+        assert(numSubarrayProductLessThanK(input, 2) == 6);
     }
     return 0;
 }
