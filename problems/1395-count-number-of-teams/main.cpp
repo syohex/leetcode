0f94dc203ecdfef684084ed7e589622cc6a46341
Shohei YOSHIDA <syohex@gmail.com>
2020-06-18 14:31:37 +0900

    Add problem 1395
    But this is still slow

---
+++ problems/1395-count-number-of-teams/main.cpp
@@ -0,0 +1,44 @@
+#include <cassert>
+#include <vector>
+#include <functional>
+#include <cstdio>
+
+int numTeams(const std::vector<int> &rating) {
+    int ret = 0;
+    std::function<void(size_t index, const std::vector<int> &v)> f;
+    f = [&f, &ret, &rating](size_t index, const std::vector<int> &v) {
+        if (v.size() == 3) {
+            if ((v[0] < v[1] && v[1] < v[2]) || (v[0] > v[1] && v[1] > v[2])) {
+                ++ret;
+            }
+            return;
+        }
+        if (index >= rating.size()) {
+            return;
+        }
+
+        auto vv = v;
+        vv.push_back(rating[index]);
+        f(index + 1, vv);
+        f(index + 1, v);
+    };
+
+    f(0, std::vector<int>{});
+    return ret;
+}
+
+int main() {
+    {
+        std::vector<int> input{2, 5, 3, 4, 1};
+        assert(numTeams(input) == 3);
+    }
+    {
+        std::vector<int> input{2, 1, 3};
+        assert(numTeams(input) == 0);
+    }
+    {
+        std::vector<int> input{1, 2, 3, 4};
+        assert(numTeams(input) == 4);
+    }
+    return 0;
+}
