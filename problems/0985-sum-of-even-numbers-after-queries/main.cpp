48f5d176fd983ed1f7efd216b00e8f8cef3180f4
Shohei YOSHIDA <syohex@gmail.com>
2020-06-21 08:04:32 +0900

    Accepted version 0985

--- problems/0985-sum-of-even-numbers-after-queries/main.cpp
+++ problems/0985-sum-of-even-numbers-after-queries/main.cpp
@@ -1,30 +1,44 @@
 #include <cassert>
 #include <vector>
 #include <set>
+#include <cstdio>
 
 std::vector<int> sumEvenAfterQueries(std::vector<int> &A, const std::vector<std::vector<int>> &queries) {
     std::set<size_t> evenSet;
+    int sum = 0;
     for (size_t i = 0; i < A.size(); ++i) {
         if (A[i] % 2 == 0) {
             evenSet.insert(i);
+            sum += A[i];
         }
     }
 
     std::vector<int> ret;
     for (const auto &v : queries) {
+        if (v[0] == 0) {
+            ret.push_back(sum);
+            continue;
+        }
+
+        auto it = evenSet.find(v[1]);
+        int orig = 0;
+        if (it != evenSet.end()) {
+            orig = A[*it];
+        }
         A[v[1]] += v[0];
         if (A[v[1]] % 2 == 0) {
             evenSet.insert(v[1]);
+            if (it == evenSet.end()) {
+                sum += A[v[1]];
+            } else {
+                sum += v[0];
+            }
+            ret.push_back(sum);
         } else {
             evenSet.erase(v[1]);
+            sum -= orig;
+            ret.push_back(sum);
         }
-
-        int sum = 0;
-        for (auto index : evenSet) {
-            sum += A[index];
-        }
-
-        ret.push_back(sum);
     }
     return ret;
 }
@@ -40,6 +54,28 @@ int main() {
         };
         auto ret = sumEvenAfterQueries(input, queries);
         assert((ret == std::vector<int>{8, 6, 2, 4}));
+    }
+    {
+        std::vector<int> input{1, 2, 0, 4};
+        std::vector<std::vector<int>> queries{
+            {5, 2},
+            {0, 2},
+            {3, 3},
+            {2, 1},
+        };
+        auto ret = sumEvenAfterQueries(input, queries);
+        assert((ret == std::vector<int>{6, 6, 2, 4}));
+    }
+    {
+        std::vector<int> input{5, 4, 0, 2};
+        std::vector<std::vector<int>> queries{
+            {1, 1},
+            {3, 1},
+            {3, 3},
+            {5, 1},
+        };
+        auto ret = sumEvenAfterQueries(input, queries);
+        assert((ret == std::vector<int>{2, 10, 8, 0}));
     }
     return 0;
 }
