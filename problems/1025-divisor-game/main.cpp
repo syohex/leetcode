b133d7754a5232163b6af466da905d0060591858
Shohei YOSHIDA <syohex@gmail.com>
2020-07-01 23:19:03 +0900

    Accepted

--- problems/1025-divisor-game/main.cpp
+++ problems/1025-divisor-game/main.cpp
@@ -1,25 +1,51 @@
 #include <cassert>
 #include <functional>
 #include <cmath>
+#include <map>
 
 bool divisorGame(int N) {
-    std::function<bool(int a, bool isAlice)> f;
-    f = [&f](int a, bool isAlice) -> bool {
-        if (a == 1) {
-            return !isAlice;
+    struct Arg {
+        explicit Arg(int n, bool turn) : n(n), isAlice(turn) {
+        }
+        int n;
+        int isAlice;
+
+        bool operator==(const Arg &other) const {
+            return n == other.n && isAlice == other.isAlice;
+        }
+        bool operator<(const Arg &other) const {
+            return n < other.n;
+        }
+    };
+
+    std::function<bool(const Arg &a)> f;
+    std::map<Arg, bool> cache;
+    f = [&f, &cache](const Arg &a) -> bool {
+        if (a.n == 1) {
+            return !a.isAlice;
         }
 
-        int limit = static_cast<int>(std::sqrt(a));
+        int limit = static_cast<int>(std::sqrt(a.n));
         int ok = 0;
         int ret = 0;
         for (int i = 1; i <= limit; ++i) {
-            if (a % i == 0) {
+            if (a.n % i == 0) {
                 ++ok;
-                if (f(a - i, !isAlice)) {
-                    ++ret;
-                    if (isAlice) {
+
+                Arg next(a.n - i, !a.isAlice);
+                bool aliceWin;
+                if (cache.find(next) != cache.end()) {
+                    aliceWin = cache[next];
+                } else {
+                    aliceWin = f(next);
+                    cache[next] = aliceWin;
+                }
+
+                if (aliceWin) {
+                    if (a.isAlice) {
                         return true;
                     }
+                    ++ret;
                 }
             }
         }
@@ -27,7 +53,7 @@ bool divisorGame(int N) {
         return ok == ret;
     };
 
-    return f(N, true);
+    return f(Arg(N, true));
 }
 
 int main() {
@@ -35,6 +61,7 @@ int main() {
     assert(!divisorGame(3));
     assert(divisorGame(4));
     assert(!divisorGame(5));
+    assert(divisorGame(12));
 
     return 0;
 }
