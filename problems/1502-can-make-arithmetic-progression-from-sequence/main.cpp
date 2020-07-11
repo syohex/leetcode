457e927dc7803d17353b34ae5563ed7efdeaa46d
Shohei YOSHIDA <syohex@gmail.com>
2020-07-11 21:33:40 +0900

    Add problem 1502

---
+++ problems/1502-can-make-arithmetic-progression-from-sequence/main.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <vector>
+#include <algorithm>
+
+bool canMakeArithmeticProgression(std::vector<int> &arr) {
+    std::sort(arr.begin(), arr.end());
+
+    int diff = arr[1] - arr[0];
+    for (size_t i = 1; i < arr.size() - 1; ++i) {
+        if (arr[i + 1] - arr[i] != diff) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main() {
+    {
+        std::vector<int> input{3, 5, 1};
+        assert(canMakeArithmeticProgression(input));
+    }
+    {
+        std::vector<int> input{1, 2, 4};
+        assert(!canMakeArithmeticProgression(input));
+    }
+    return 0;
+}
