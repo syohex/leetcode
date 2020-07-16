624ed2c37b191f86c936d37ef1f2e1a2dfcf8be5
Shohei YOSHIDA <syohex@gmail.com>
2020-07-16 23:55:44 +0900

    Add problem 0598

---
+++ problems/0598-range-addition-2/main.cpp
@@ -0,0 +1,34 @@
+#include <cassert>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+int maxCount(int m, int n, const std::vector<std::vector<int>>& ops) {
+    if (ops.empty()) {
+        return m * n;
+    }
+
+    int minX = INT_MAX, minY = INT_MAX;
+    for (const auto& op : ops) {
+        minX = std::min(minX, op[0]);
+        minY = std::min(minY, op[1]);
+    }
+
+    return minX * minY;
+}
+
+
+int main() {
+    {
+        std::vector<std::vector<int>> ops {
+            {2,2},
+            {3,3},
+        };
+        assert(maxCount(3, 3, ops) == 4);
+    }
+    {
+        std::vector<std::vector<int>> ops;
+        assert(maxCount(3, 3, ops) == 9);
+    }
+    return 0;
+}
