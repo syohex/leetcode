7d2aaa00257253895b4eb777634c6836df711b6f
Shohei YOSHIDA <syohex@gmail.com>
2020-08-03 12:16:57 +0900

    Accepted

--- problems/1128-number-of-equivalent-domino-pairs/main.cpp
+++ problems/1128-number-of-equivalent-domino-pairs/main.cpp
@@ -1,18 +1,30 @@
 #include <cassert>
 #include <vector>
+#include <set>
 
 int numEquivDominoPairs(const std::vector<std::vector<int>> &dominoes) {
     if (dominoes.size() == 1) {
         return 0;
     }
 
+    std::set<size_t> checked;
     int ret = 0;
     for (size_t i = 0; i < dominoes.size() - 1; ++i) {
+        if (checked.find(i) != checked.end()) {
+            continue;
+        }
+
+        int n = 0;
         for (size_t j = i + 1; j < dominoes.size(); ++j) {
             if ((dominoes[i][0] == dominoes[j][0] && dominoes[i][1] == dominoes[j][1]) ||
                 (dominoes[i][0] == dominoes[j][1] && dominoes[i][1] == dominoes[j][0])) {
-                ++ret;
+                ++n;
+                checked.insert(j);
             }
+        }
+
+        if (n != 0) {
+            ret += (n * (1 + n)) / 2;
         }
     }
 
