9a5acadbc3f6eb4e99c2f47f9a6f0348d3443fe1
Shohei YOSHIDA <syohex@gmail.com>
2020-08-17 12:23:00 +0900

    Add problem 1496

---
+++ problems/1496-path-crossing/main.cpp
@@ -0,0 +1,40 @@
+#include <cassert>
+#include <string>
+#include <set>
+#include <utility>
+
+bool isPathCrossing(const std::string &path) {
+    std::set<std::pair<int, int>> s{std::make_pair(0, 0)};
+
+    std::pair<int, int> p(0, 0);
+    for (const auto c : path) {
+        switch (c) {
+        case 'N':
+            ++p.second;
+            break;
+        case 'S':
+            --p.second;
+            break;
+        case 'E':
+            ++p.first;
+            break;
+        case 'W':
+            --p.first;
+            break;
+        }
+
+        if (s.find(p) != s.end()) {
+            return true;
+        }
+
+	s.insert(p);
+    }
+
+    return false;
+}
+
+int main() {
+    assert(!isPathCrossing("NES"));
+    assert(isPathCrossing("NESWW"));
+    return 0;
+}
