cdc671f1227f07286fbae5fc2ac23f9d66205088
Shohei YOSHIDA <syohex@gmail.com>
2020-08-27 11:42:57 +0900

    Speed up but not accepted

--- problems/0696-count-binary-substrings/main.cpp
+++ problems/0696-count-binary-substrings/main.cpp
@@ -8,22 +8,27 @@ int countBinarySubstrings(const std::string &s) {
     }
 
     int ret = 0;
-    for (size_t i = 0; i < limit - 1; ++i) {
+    for (size_t i = 0; i < limit - 1;) {
         int count1 = 1;
         size_t j = i + 1;
         while (j < limit && s[i] == s[j]) {
             ++j;
             ++count1;
         }
+        int tmp = j;
         int count2 = 0;
         while (j < limit && s[i] != s[j]) {
             ++count2;
             if (count2 == count1) {
-                ++ret;
-                break;
+                i = tmp;
+                ret += count1;
+                goto next;
             }
             ++j;
         }
+
+        ++i;
+    next:;
     }
     return ret;
 }
