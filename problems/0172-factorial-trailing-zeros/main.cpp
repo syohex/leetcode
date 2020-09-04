504699837912111a1a62dbd2f5124f972f5e4ad9
Shohei YOSHIDA <syohex@gmail.com>
2020-09-04 22:40:56 +0900

    Add problem 0172 but too slow yet

---
+++ problems/0172-factorial-trailing-zeros/main.cpp
@@ -0,0 +1,38 @@
+#include <cassert>
+#include <cstdio>
+
+int trailingZeroes(int n) {
+    int ret = 0;
+    for (int i = 2; i <= n; ++i) {
+        int j = i;
+        while (j > 0) {
+            if (j % 10 == 0) {
+                ++ret;
+                j /= 10;
+                continue;
+            }
+
+            break;
+        }
+        while (j > 0) {
+            if (j % 5 == 0) {
+                ++ret;
+                j /= 5;
+                continue;
+            }
+
+            break;
+        }
+    }
+
+    return ret;
+}
+
+int main() {
+    assert(trailingZeroes(3) == 0);
+    assert(trailingZeroes(5) == 1);
+    assert(trailingZeroes(20) == 4);
+    assert(trailingZeroes(100) == 24);
+    assert(trailingZeroes(123) == 28);
+    return 0;
+}
