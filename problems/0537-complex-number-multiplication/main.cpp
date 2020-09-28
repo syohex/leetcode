aaa5e338320819c481de1f27462a7c3596597afe
Shohei YOSHIDA <syohex@gmail.com>
2020-09-29 08:45:35 +0900

    Add problem 0537

---
+++ problems/0537-complex-number-multiplication/main.cpp
@@ -0,0 +1,25 @@
+#include <cassert>
+#include <string>
+#include <cstring>
+#include <cstdio>
+
+std::string complexNumberMultiply(const std::string& a, const std::string& b) {
+    struct Complex {
+        int real;
+        int image;
+    };
+
+    Complex ac, bc;
+
+    sscanf(a.c_str(), "%d+%di", &ac.real, &ac.image);
+    sscanf(b.c_str(), "%d+%di", &bc.real, &bc.image);
+
+    Complex ret{ac.real * bc.real - ac.image * bc.image, ac.real * bc.image + ac.image * bc.real};
+    return std::to_string(ret.real) + "+" + std::to_string(ret.image) + "i";
+}
+
+int main() {
+    assert(complexNumberMultiply("1+1i", "1+1i") == "0+2i");
+    assert(complexNumberMultiply("1+-1i", "1+-1i") == "0+-2i");
+    return 0;
+}
