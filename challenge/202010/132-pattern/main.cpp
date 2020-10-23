841d94de0f73d9dce5c9ce625c85c9d94fe6c107
Shohei YOSHIDA <syohex@gmail.com>
2020-10-23 16:59:08 +0900

    Add O(n^2) solution

--- challenge/202010/132-pattern/main.cpp
+++ challenge/202010/132-pattern/main.cpp
@@ -1,6 +1,8 @@
 #include <cassert>
 #include <vector>
 #include <cstdio>
+#include <algorithm>
+#include <climits>
 
 bool find132pattern(const std::vector<int> &nums) {
     if (nums.size() < 3) {
@@ -24,9 +26,34 @@ bool find132pattern(const std::vector<int> &nums) {
     return false;
 }
 
+bool find132pattern2(const std::vector<int> &nums) {
+    if (nums.size() < 3) {
+        return false;
+    }
+
+    int min_i_val = nums[0];
+    for (size_t j = 1; j < nums.size() - 1; ++j) {
+        min_i_val = std::min(min_i_val, nums[j]);
+        if (min_i_val >= nums[j]) {
+            continue;
+        }
+        for (size_t k = j + 1; k < nums.size(); ++k) {
+            if (min_i_val < nums[k] && nums[k] < nums[j]) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
 int main() {
     assert(!find132pattern(std::vector<int>{1, 2, 3, 4}));
     assert(find132pattern(std::vector<int>{3, 1, 4, 2}));
     assert(find132pattern(std::vector<int>{-1, 3, 2, 0}));
+
+    assert(!find132pattern2(std::vector<int>{1, 2, 3, 4}));
+    assert(find132pattern2(std::vector<int>{3, 1, 4, 2}));
+    assert(find132pattern2(std::vector<int>{-1, 3, 2, 0}));
     return 0;
 }
