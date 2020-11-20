cbbcd76604deac7a31d4015fbdaf607166c3a9bc
Shohei YOSHIDA <syohex@gmail.com>
2020-11-20 11:49:35 +0900

    Add contains duplicate

---
+++ interview/top_interview_questions/array/contains-duplicate/main.cpp
@@ -0,0 +1,24 @@
+#include <cassert>
+#include <vector>
+#include <unordered_set>
+
+bool containsDuplicate(const std::vector<int> &nums) {
+    std::unordered_set<int> s;
+    for (const auto n : nums) {
+        if (s.find(n) != s.end()) {
+            return true;
+        }
+
+        s.insert(n);
+    }
+
+    return false;
+}
+
+int main() {
+    assert((containsDuplicate(std::vector<int>{1, 2, 3, 1})));
+    assert((!containsDuplicate(std::vector<int>{1})));
+    assert((!containsDuplicate(std::vector<int>{1, 2, 3, 4})));
+    assert((containsDuplicate(std::vector<int>{1, 1, 1, 3, 3, 4, 3, 2, 4, 2})));
+    return 0;
+}
