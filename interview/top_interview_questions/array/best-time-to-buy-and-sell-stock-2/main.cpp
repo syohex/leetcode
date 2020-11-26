b2f6595ff789dae27999c03ab51b5ff502fb4856
Shohei YOSHIDA <syohex@gmail.com>
2020-11-26 15:31:17 +0900

    Add best time to buy and sell stock 2

---
+++ interview/top_interview_questions/array/best-time-to-buy-and-sell-stock-2/main.cpp
@@ -0,0 +1,43 @@
+#include <cassert>
+#include <vector>
+
+int maxProfit(const std::vector<int> &prices) {
+    int i = 0;
+    int limit = static_cast<int>(prices.size());
+
+    int ret = 0;
+    while (i < limit) {
+        while (i < limit - 1 && prices[i] > prices[i + 1]) {
+            ++i;
+        }
+
+        int low = prices[i];
+
+        while (i < limit - 1 && prices[i] < prices[i + 1]) {
+            ++i;
+        }
+
+        int high = prices[i];
+        ret += high - low;
+
+        ++i;
+    }
+
+    return ret;
+}
+
+int main() {
+    {
+        std::vector<int> prices{7, 1, 5, 3, 6, 4};
+        assert(maxProfit(prices) == 7);
+    }
+    {
+        std::vector<int> prices{1, 2, 3, 4, 5};
+        assert(maxProfit(prices) == 4);
+    }
+    {
+        std::vector<int> prices{7, 6, 4, 3, 1};
+        assert(maxProfit(prices) == 0);
+    }
+    return 0;
+}
