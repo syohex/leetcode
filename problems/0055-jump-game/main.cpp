dc2a318921a789701b6b3a7f40b74a24d828bb5a
Shohei YOSHIDA <syohex@gmail.com>
2020-11-30 11:35:56 +0900

    Accepted

--- problems/0055-jump-game/main.cpp
+++ problems/0055-jump-game/main.cpp
@@ -1,8 +1,10 @@
 #include <cassert>
 #include <vector>
 #include <stack>
+#include <algorithm>
+#include <cstdio>
 
-bool canJump(const std::vector<int> &nums) {
+bool canJump(std::vector<int> &nums) {
     size_t end = nums.size() - 1;
     std::stack<size_t> pos_candidates;
     pos_candidates.push(0);
@@ -16,12 +18,16 @@ bool canJump(const std::vector<int> &nums) {
             return true;
         }
 
+        nums[pos] = -1;
+
         if (moves == 0) {
             continue;
         }
 
         for (int i = 1; i <= moves; ++i) {
-            pos_candidates.push(pos + i);
+            if (nums[pos + i] > 0) {
+                pos_candidates.push(pos + i);
+            }
         }
     }
 
@@ -35,6 +41,17 @@ int main() {
     }
     {
         std::vector<int> nums{3, 2, 1, 0, 4};
+        assert(!canJump(nums));
+    }
+    {
+        std::vector<int> nums{5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0};
+        assert(canJump(nums));
+    }
+    {
+        std::vector<int> nums{2, 0, 6, 9, 8, 4, 5, 0, 8, 9, 1, 2, 9, 6, 8, 8, 0, 6, 3, 1, 2, 2, 1, 2, 6, 5,
+                              3, 1, 2, 2, 6, 4, 2, 4, 3, 0, 0, 0, 3, 8, 2, 4, 0, 1, 2, 0, 1, 4, 6, 5, 8, 0,
+                              7, 9, 3, 4, 6, 6, 5, 8, 9, 3, 4, 3, 7, 0, 4, 9, 0, 9, 8, 4, 3, 0, 7, 7, 1, 9,
+                              1, 9, 4, 9, 0, 1, 9, 5, 7, 7, 1, 5, 8, 2, 8, 2, 6, 8, 2, 2, 7, 5, 1, 7, 9, 6};
         assert(!canJump(nums));
     }
     return 0;
