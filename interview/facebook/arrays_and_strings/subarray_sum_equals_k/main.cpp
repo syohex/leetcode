19edc95b33567e8a98a8b2552934701ae4049399
Shohei YOSHIDA <syohex@gmail.com>
2020-12-11 13:30:28 +0900

    Accepted

--- interview/facebook/arrays_and_strings/subarray_sum_equals_k/main.cpp
+++ interview/facebook/arrays_and_strings/subarray_sum_equals_k/main.cpp
@@ -1,20 +1,26 @@
 #include <cassert>
 #include <vector>
+#include <map>
 
 int subarraySum(const std::vector<int> &nums, int k) {
-    std::vector<int> sums(nums.size() + 1);
-    sums[0] = 0;
-    for (size_t i = 1; i <= nums.size(); ++i) {
-        sums[i] = nums[i - 1] + sums[i - 1];
+    std::vector<int> sums(nums.size());
+    sums[0] = nums[0];
+    for (size_t i = 1; i < nums.size(); ++i) {
+        sums[i] = nums[i] + sums[i - 1];
     }
 
+    std::map<int, int> m;
     int ret = 0;
-    for (size_t i = 1; i <= nums.size(); ++i) {
-        for (size_t j = 0; j < i; ++j) {
-            if (sums[i] - sums[j] == k) {
-                ++ret;
-            }
+    for (size_t i = 0; i < nums.size(); ++i) {
+        int diff = sums[i] - k;
+        if (m.find(diff) != m.end()) {
+            ret += m[diff];
         }
+        if (diff == 0) {
+            ++ret;
+        }
+
+        ++m[sums[i]];
     }
     return ret;
 }
