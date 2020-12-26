2a260ceafba5c064d3e482341d1ee2dfe347a660
Shohei YOSHIDA <syohex@gmail.com>
2020-12-26 11:40:33 +0900

    Accepted

--- problems/0539-mimum-time-difference/main.cpp
+++ problems/0539-mimum-time-difference/main.cpp
@@ -11,32 +11,55 @@ int findMinDifference(const std::vector<std::string> &timePoints) {
         return hours * 60 + min;
     };
 
-    std::vector<int> times;
+    std::vector<int> minutes(60 * 24, 0);
     for (const auto &t : timePoints) {
-        times.push_back(to_minutes(t));
+        int min = to_minutes(t);
+        if (minutes[min] != 0) {
+            return 0;
+        }
+
+        ++minutes[to_minutes(t)];
     }
 
     constexpr int one_day = 60 * 24;
     constexpr int half_day = 60 * 12;
     int ret = INT_MAX;
-    for (size_t i = 0; i < times.size() - 1; ++i) {
-        for (size_t j = i + 1; j < times.size(); ++j) {
-            int diff = std::abs(times[i] - times[j]);
+
+    int first = -1;
+    int prev = 0;
+    for (int i = 0; i < 1440; ++i) {
+        if (minutes[i] == 0) {
+            continue;
+        }
+        if (first == -1) {
+            first = i;
+            prev = i;
+        } else {
+            int diff = std::abs(i - prev);
             if (diff >= half_day) {
                 diff = one_day - diff;
             }
 
             ret = std::min(ret, diff);
+            prev = i;
         }
     }
 
-    return ret;
+    int diff = std::abs(prev - first);
+    if (diff >= half_day) {
+        diff = one_day - diff;
+    }
+    return std::min(ret, diff);
 }
 
 int main() {
     {
         std::vector<std::string> input{"23:59", "00:00"};
         assert(findMinDifference(input) == 1);
+    }
+    {
+        std::vector<std::string> input{"01:01", "02:02"};
+        assert(findMinDifference(input) == 61);
     }
     {
         std::vector<std::string> input{"00:00", "23:59", "00:00"};
