d346c03a769920a3dbc1beb1d4162cc1af597d65
Shohei YOSHIDA <syohex@gmail.com>
2021-01-03 17:27:12 +0900

    Accepted. improve used value check.

--- challenge/202101/beautiful_arrangement/main.cpp
+++ challenge/202101/beautiful_arrangement/main.cpp
@@ -1,37 +1,38 @@
 #include <cassert>
 #include <functional>
-#include <set>
+#include <vector>
 #include <cstdio>
 
 int countArangement(int n) {
     int ret = 0;
-    std::function<void(int m, std::set<int> used)> f;
-    f = [&f, &n, &ret](int m, std::set<int> used) {
+    std::function<void(int m, std::vector<bool> &used)> f;
+    f = [&f, &n, &ret](int m, std::vector<bool> &used) {
         if (m > n) {
             ++ret;
             return;
         }
 
         for (int i = 1; i <= n; ++i) {
-            if (used.find(i) != used.end()) {
+            if (used[i - 1]) {
                 continue;
             }
 
             if (i % m == 0 || m % i == 0) {
-                auto tmp = used;
-                tmp.insert(i);
-                f(m + 1, tmp);
+                used[i - 1] = true;
+                f(m + 1, used);
+                used[i - 1] = false;
             }
         }
     };
 
-    f(1, std::set<int>{});
+    std::vector<bool> used(n, false);
+    f(1, used);
     return ret;
 }
 
 int main() {
     assert(countArangement(1) == 1);
     assert(countArangement(2) == 2);
-    assert(countArangement(15) == 2);
+    assert(countArangement(15) == 24679);
     return 0;
 }
