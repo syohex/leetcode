ed5bbf42cd0448aa3e78aaf9dfa8f2f04dfcbfbb
Shohei YOSHIDA <syohex@gmail.com>
2021-02-03 17:09:14 +0900

    Add linked list cycle

---
+++ challenge/202102/linked_list_cycle/main.cpp
@@ -0,0 +1,60 @@
+#include <cassert>
+#include <set>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {
+    }
+    ~ListNode() {
+        delete next;
+    }
+};
+
+bool hasCycle(ListNode *head) {
+    if (head == nullptr) {
+        return false;
+    }
+
+    ListNode *p = head;
+    std::set<ListNode *> s;
+    while (p != nullptr) {
+        if (s.find(p) != s.end()) {
+            return true;
+        }
+
+        s.insert(p);
+        p = p->next;
+    }
+
+    return false;
+}
+
+int main() {
+    {
+        ListNode *r = new ListNode(3);
+        r->next = new ListNode(2);
+        r->next->next = new ListNode(0);
+        r->next->next->next = new ListNode(4);
+        r->next->next->next->next = r->next;
+
+        assert(hasCycle(r));
+        r->next->next->next->next = nullptr;
+        delete r;
+    }
+    {
+        ListNode *r = new ListNode(1);
+        r->next = new ListNode(2);
+        r->next->next = r;
+
+        assert(hasCycle(r));
+        r->next->next = nullptr;
+        delete r;
+    }
+    {
+        ListNode *r = new ListNode(1);
+        assert(!hasCycle(r));
+        delete r;
+    }
+    return 0;
+}
