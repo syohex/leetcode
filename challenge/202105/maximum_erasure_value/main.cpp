1209853824a0925bef2762ba7110a68599f4b3f3
Shohei YOSHIDA <syohex@gmail.com>
2021-05-28 18:01:39 +0900

    accepted

--- challenge/202105/maximum_erasure_value/main.cpp
+++ challenge/202105/maximum_erasure_value/main.cpp
@@ -2,25 +2,33 @@
 #include <vector>
 #include <algorithm>
 #include <set>
+#include <map>
 
 int maximumUniqueSubarray(const std::vector<int> &nums) {
-    int ret = 0;
-    int lens = nums.size();
-    for (int i = 0; i < lens; ++i) {
-        std::set<int> s;
-        s.insert(nums[i]);
-        int sum = nums[i];
-        for (int j = i + 1; j < lens; ++j) {
-            if (s.find(nums[j]) != s.end()) {
-                break;
-            }
+    std::vector<int> accs(nums.size() + 1);
+    accs[0] = 0;
+    for (size_t i = 0; i < nums.size(); ++i) {
+        accs[i + 1] = nums[i] + accs[i];
+    }
 
-            sum += nums[j];
-            s.insert(nums[j]);
+    std::map<size_t, size_t> m;
+    size_t min_pos = 0;
+    std::vector<size_t> unique_start(nums.size() + 1);
+    for (size_t i = 0; i < nums.size(); ++i) {
+        if (m.find(nums[i]) != m.end()) {
+            min_pos = std::max(min_pos, m[nums[i]] + 1);
         }
 
-        ret = std::max(ret, sum);
+        unique_start[i] = min_pos;
+        m[nums[i]] = i;
     }
+
+    int ret = 0;
+    for (size_t i = 1; i <= nums.size(); ++i) {
+        int val = accs[i] - accs[unique_start[i - 1]];
+        ret = std::max(ret, val);
+    }
+
     return ret;
 }
 
@@ -32,6 +40,10 @@ int main() {
     {
         std::vector<int> nums{5, 2, 1, 2, 5, 2, 1, 2, 5};
         assert(maximumUniqueSubarray(nums) == 8);
+    }
+    {
+        std::vector<int> nums{10000, 1, 10000, 1, 1, 1, 1, 1, 1};
+        assert(maximumUniqueSubarray(nums) == 10001);
     }
     return 0;
 }
