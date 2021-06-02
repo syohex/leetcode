953e2f9223282dfc5536a034e15e0d6f7966ae35
Shohei YOSHIDA <syohex@gmail.com>
2021-06-02 16:26:36 +0900

    Accepted with cache

--- challenge/202106/interleaving_string/main.cpp
+++ challenge/202106/interleaving_string/main.cpp
@@ -1,16 +1,24 @@
 #include <cassert>
 #include <string>
 #include <functional>
+#include <set>
 
 bool isInterleave(const std::string &s1, const std::string &s2, const std::string &s3) {
     if (s1.size() + s2.size() != s3.size()) {
         return false;
     }
 
+    std::set<std::tuple<size_t, size_t, size_t>> cache;
+
     std::function<bool(size_t p1, size_t p2, size_t p)> f;
-    f = [&f, &s1, &s2, &s3](size_t p1, size_t p2, size_t p) -> bool {
+    f = [&f, &s1, &s2, &s3, &cache](size_t p1, size_t p2, size_t p) -> bool {
         if (p == s3.size()) {
             return true;
+        }
+
+        const std::tuple<size_t, size_t, size_t> key{p1, p2, p};
+        if (cache.find(key) != cache.end()) {
+            return false;
         }
 
         if (p1 < s1.size() && s1[p1] == s3[p]) {
@@ -25,6 +33,7 @@ bool isInterleave(const std::string &s1, const std::string &s2, const std::strin
             }
         }
 
+        cache.insert(key);
         return false;
     };
 
