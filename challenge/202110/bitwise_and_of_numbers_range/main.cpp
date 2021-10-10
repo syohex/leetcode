b5b082ad59ec4e4bf030d6ef05fa7aa5c94f1d3c
Shohei YOSHIDA <syohex@gmail.com>
2021-10-10 19:06:48 +0900

    Add bitwise and of numbers range

---
+++ challenge/202110/bitwise_and_of_numbers_range/main.cpp
@@ -0,0 +1,18 @@
+#include <cassert>
+
+int rangeBitwiseAnd(int left, int right) {
+    int shifts = 0;
+    while (left < right) {
+        left >>= 1;
+        right >>= 1;
+        ++shifts;
+    }
+    return left << shifts;
+}
+
+int main() {
+    assert(rangeBitwiseAnd(5, 7) == 4);
+    assert(rangeBitwiseAnd(0, 0) == 0);
+    assert(rangeBitwiseAnd(1, 2147483647) == 0);
+    return 0;
+}
