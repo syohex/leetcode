09a3f93941ca8f04fe9920e6ca63ad10835c8a64
Shohei YOSHIDA <syohex@gmail.com>
2021-10-21 12:02:52 +0900

    Add insert delete getrandom O(1)

--- challenge/202110/insert_delete_getrandom/main.cpp
+++ challenge/202110/insert_delete_getrandom/main.cpp
@@ -1,47 +1,52 @@
 #include <cassert>
+#include <map>
+#include <vector>
 #include <random>
-#include <set>
+#include <algorithm>
 
 class RandomizedSet {
   public:
-    RandomizedSet() : mt_(rnd_()) {
+    RandomizedSet() : array_(1'000'000), pos_(0), mt_(rnd_()) {
     }
 
     bool insert(int val) {
-        if (set_.find(val) != set_.end()) {
+        if (map_.find(val) != map_.end()) {
             return false;
         }
 
-        set_.insert(val);
+        map_[val] = pos_;
+        array_[pos_] = val;
+        ++pos_;
         return true;
     }
 
     bool remove(int val) {
-        if (set_.find(val) == set_.end()) {
+        if (map_.find(val) == map_.end()) {
             return false;
         }
 
-        set_.erase(val);
+        int last_val = array_[pos_ - 1];
+        size_t index = map_[val];
+
+        array_[index] = last_val;
+        map_[last_val] = index;
+
+        --pos_;
+        map_.erase(val);
         return true;
     }
 
     int getRandom() {
-        std::uniform_int_distribution<int> dist(0, static_cast<int>(set_.size() - 1));
+        std::uniform_int_distribution<int> dist(0, static_cast<int>(map_.size() - 1));
         int index = dist(mt_);
-        int i = 0;
-        for (auto it = set_.begin(); it != set_.end(); ++it, ++i) {
-            if (i == index) {
-                return *it;
-            }
-        }
-
-        // never reach here
-        return -1;
+        return array_[index];
     }
 
+    std::map<int, size_t> map_;
+    std::vector<int> array_;
+    size_t pos_;
     std::random_device rnd_;
     std::mt19937 mt_;
-    std::set<int> set_;
 };
 
 int main() {
@@ -55,6 +60,16 @@ int main() {
         assert(r.remove(1));
         assert(!r.insert(2));
         ret = r.getRandom();
+        assert(ret == 2);
+    }
+    {
+        RandomizedSet r;
+        assert(r.insert(0));
+        assert(r.insert(1));
+        assert(r.remove(0));
+        assert(r.insert(2));
+        assert(r.remove(1));
+        auto ret = r.getRandom();
         assert(ret == 2);
     }
     return 0;
