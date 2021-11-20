0bd54d1a895c0318ef6498d22918601d11e642bc
Shohei YOSHIDA <syohex@gmail.com>
2021-11-20 13:20:30 +0900

    Add single element in a sorted array

---
+++ challenge/202111/single_element_in_a_sorted_array/main.cpp
@@ -0,0 +1,34 @@
+#include <cassert>
+#include <vector>
+
+int singleNonDuplicate(const std::vector<int> &nums) {
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (mid % 2 == 1) {
+            --mid;
+        }
+
+        if (nums[mid] == nums[mid + 1]) {
+            left = mid + 2;
+        } else {
+            right = mid;
+        }
+    }
+
+    return nums[left];
+}
+
+int main() {
+    {
+        std::vector<int> nums{1, 1, 2, 3, 3, 4, 4, 8, 8};
+        assert(singleNonDuplicate(nums) == 2);
+    }
+    {
+        std::vector<int> nums{3, 3, 7, 7, 10, 11, 11};
+        assert(singleNonDuplicate(nums) == 10);
+    }
+    return 0;
+}
