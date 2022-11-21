03246203432f76f3f65591c05c77c030f03f47a3
Shohei YOSHIDA <syohex@gmail.com>
2022-11-21 10:46:15 +0900

    Add priority queue and accepted

--- challenge/202211/nearest_exit_from_entrance_in_maze/main.cpp
+++ challenge/202211/nearest_exit_from_entrance_in_maze/main.cpp
@@ -1,25 +1,40 @@
 #include <cassert>
 #include <vector>
-#include <functional>
 #include <set>
-#include <numeric>
-#include <algorithm>
+#include <queue>
 
 int nearestExit(const std::vector<std::vector<char>> &maze, const std::vector<int> &entrance) {
     int rows = maze.size();
     int cols = maze[0].size();
     std::vector<std::vector<int>> steps{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
-    std::function<int(int row, int col, int n, std::set<std::vector<int>> &visited)> f;
-    f = [&](int row, int col, int n, std::set<std::vector<int>> &visited) -> int {
-        if (row < 0 || row >= rows || col < 0 || col >= cols) {
-            return n - 1;
+    struct Data {
+        int row;
+        int col;
+        int step;
+    };
+    const auto cmp = [](const Data &a, const Data &b) -> bool { return a.step > b.step; };
+
+    std::priority_queue<Data, std::vector<Data>, decltype(cmp)> q(cmp);
+    q.push({entrance[0], entrance[1], 0});
+
+    std::set<std::vector<int>> visited{entrance};
+    while (!q.empty()) {
+        Data d = q.top();
+        q.pop();
+
+        if (d.row < 0 || d.row >= rows || d.col < 0 || d.col >= cols) {
+            if (d.step == 1) {
+                // entrance is exit
+                continue;
+            }
+
+            return d.step - 1;
         }
 
-        int ret = std::numeric_limits<int>::max();
         for (const auto &step : steps) {
-            int new_row = row + step[0];
-            int new_col = col + step[1];
+            int new_row = d.row + step[0];
+            int new_col = d.col + step[1];
 
             std::vector<int> key{new_row, new_col};
             if (new_row >= 0 && new_row < rows && new_col >= 0 && new_col < cols) {
@@ -33,19 +48,11 @@ int nearestExit(const std::vector<std::vector<char>> &maze, const std::vector<in
             }
 
             visited.insert(key);
-            int r = f(new_row, new_col, n + 1, visited);
-            if (r > 0) {
-                ret = std::min(ret, r);
-            }
-            visited.erase(key);
+            q.push({new_row, new_col, d.step + 1});
         }
+    }
 
-        return ret;
-    };
-
-    std::set<std::vector<int>> visited{entrance};
-    int ret = f(entrance[0], entrance[1], 0, visited);
-    return ret == std::numeric_limits<int>::max() ? -1 : ret;
+    return -1;
 }
 
 int main() {
