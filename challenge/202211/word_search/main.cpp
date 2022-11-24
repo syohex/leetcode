16bd60185564bde2fa4097fe310a629ea41d2b8b
Shohei YOSHIDA <syohex@gmail.com>
2022-11-24 11:16:19 +0900

    Accepted

--- challenge/202211/word_search/main.cpp
+++ challenge/202211/word_search/main.cpp
@@ -10,19 +10,19 @@ bool exist(const std::vector<std::vector<char>> &board, const std::string &word)
     int cols = board[0].size();
 
     std::vector<std::vector<int>> steps{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-    std::function<bool(int row, int col, size_t pos, std::set<std::vector<int>> &visited)> f;
-    f = [&](int row, int col, size_t pos, std::set<std::vector<int>> &visited) -> bool {
+    std::function<bool(int row, int col, size_t pos, std::vector<std::vector<bool>> &visited)> f;
+    f = [&](int row, int col, size_t pos, std::vector<std::vector<bool>> &visited) -> bool {
         if (pos == word.size()) {
             return true;
         }
 
+        visited[row][col] = true;
         for (const auto &step : steps) {
             int new_row = row + step[0];
             int new_col = col + step[1];
 
             if (new_row >= 0 && new_row < rows && new_col >= 0 && new_col < cols) {
-                std::vector<int> key{new_row, new_col};
-                if (visited.find(key) != visited.end()) {
+                if (visited[new_row][new_col]) {
                     continue;
                 }
 
@@ -30,27 +30,22 @@ bool exist(const std::vector<std::vector<char>> &board, const std::string &word)
                     continue;
                 }
 
-                visited.insert(key);
                 if (f(new_row, new_col, pos + 1, visited)) {
                     return true;
                 }
-                visited.erase(key);
             }
         }
-
+        visited[row][col] = false;
         return false;
     };
 
-    std::set<std::vector<int>> visited;
+    std::vector<std::vector<bool>> visited(rows, std::vector<bool>(cols, false));
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
             if (board[i][j] == word[0]) {
-                std::vector<int> key{i, j};
-                visited.insert(key);
                 if (f(i, j, 1, visited)) {
                     return true;
                 }
-                visited.erase(key);
             }
         }
     }
