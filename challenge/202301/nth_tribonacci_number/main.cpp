83a3f72dd2dbf4c865b52cc1f752afdeb370af2e
Shohei YOSHIDA <syohex@gmail.com>
2023-01-30 10:17:38 +0900

    Add nth tribonacci number

---
+++ challenge/202301/nth_tribonacci_number/main.cpp
@@ -0,0 +1,32 @@
+#include <cassert>
+
+int tribonacci(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+
+    int prev3 = 0;
+    int prev2 = 1;
+    int prev1 = 1;
+    int ret = 0;
+    for (int i = 3; i <= n; ++i) {
+        ret = prev1 + prev2 + prev3;
+        prev3 = prev2;
+        prev2 = prev1;
+        prev1 = ret;
+    }
+
+    return ret;
+}
+
+int main() {
+    assert(tribonacci(0) == 0);
+    assert(tribonacci(1) == 1);
+    assert(tribonacci(2) == 1);
+    assert(tribonacci(4) == 4);
+    assert(tribonacci(25) == 1389537);
+    return 0;
+}
