3db3a4cd6921e2575a24a2a6975541c2e384f5bd
Shohei YOSHIDA <syohex@gmail.com>
2023-02-10 10:49:53 +0900

    Accepted

--- challenge/202302/as_far_from_land_as_possible/main.cpp
+++ challenge/202302/as_far_from_land_as_possible/main.cpp
@@ -1,34 +1,45 @@
 #include <cassert>
 #include <vector>
-#include <algorithm>
-#include <set>
-#include <cmath>
-#include <limits>
+#include <queue>
+#include <cstdio>
 
 int maxDistance(const std::vector<std::vector<int>> &grid) {
     int n = grid.size();
-    std::set<std::pair<int, int>> lands;
+    std::queue<std::pair<int, int>> q;
+    std::vector<std::vector<int>> visited(n, std::vector<int>(n, 0));
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             if (grid[i][j] == 1) {
-                lands.insert({i, j});
+                q.push({i, j});
+                visited[i][j] = 1;
             }
         }
     }
 
-    if (lands.empty() || lands.size() == n * n) {
+    if (q.size() == 0 || q.size() == n * n) {
         return -1;
     }
 
-    int ret = 0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            int distance = std::numeric_limits<int>::max();
-            for (const auto &[row, col] : lands) {
-                distance = std::min(distance, std::abs(i - row) + std::abs(j - col));
+    std::vector<std::pair<int, int>> steps{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+    int ret = -1;
+    while (!q.empty()) {
+        size_t len = q.size();
+        for (size_t i = 0; i < len; ++i) {
+            auto [row, col] = q.front();
+            q.pop();
+
+            for (const auto &[i, j] : steps) {
+                int r = row + i;
+                int c = col + j;
+                if (r >= 0 && r < n && c >= 0 && c < n && grid[r][c] == 0 && visited[r][c] == 0) {
+                    q.push({r, c});
+                    visited[r][c] = 1;
+                }
             }
-            ret = std::max(ret, distance);
         }
+
+        ++ret;
     }
 
     return ret;
