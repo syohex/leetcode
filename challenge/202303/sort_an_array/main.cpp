388244d23d0b28f68d96265b687ad4702aed1477
Shohei YOSHIDA <syohex@gmail.com>
2023-03-01 10:28:09 +0900

    Improve a little

--- challenge/202303/sort_an_array/main.cpp
+++ challenge/202303/sort_an_array/main.cpp
@@ -3,39 +3,40 @@
 #include <functional>
 #include <cstdio>
 
-std::vector<int> sortArray(const std::vector<int> &nums) {
-    std::function<std::vector<int>(const std::vector<int> &nums)> f;
-    f = [&](const std::vector<int> &nums) -> std::vector<int> {
-        if (nums.size() == 1) {
-            return nums;
+std::vector<int> sortArray(std::vector<int> &nums) {
+    std::function<void(size_t left, size_t right)> f;
+    f = [&](size_t left, size_t right) {
+        if (left == right) {
+            return;
         }
 
-        size_t len = nums.size();
+        size_t len = right - left + 1;
         size_t half = len / 2;
-        auto left = f(std::vector<int>(nums.begin(), nums.begin() + half));
-        auto right = f(std::vector<int>(nums.begin() + half, nums.end()));
+        size_t left_end = left + half - 1;
+        size_t right_start = left + half;
+        f(left, left_end);
+        f(right_start, right);
 
-        std::vector<int> ret;
-        for (size_t i = 0, j = 0; i < left.size() || j < right.size();) {
-            if (i >= left.size()) {
-                ret.push_back(right[j]);
-                ++j;
-            } else if (j >= right.size()) {
-                ret.push_back(left[i]);
-                ++i;
-            } else if (left[i] < right[j]) {
-                ret.push_back(left[i]);
-                ++i;
+        std::vector<int> tmp;
+        for (size_t i = left, j = right_start; i <= left_end || j <= right;) {
+            if (i > left_end) {
+                tmp.push_back(nums[j++]);
+            } else if (j > right) {
+                tmp.push_back(nums[i++]);
+            } else if (nums[i] < nums[j]) {
+                tmp.push_back(nums[i++]);
             } else {
-                ret.push_back(right[j]);
-                ++j;
+                tmp.push_back(nums[j++]);
             }
         }
 
-        return ret;
+        for (size_t i = left, j = 0; i <= right; ++i, ++j) {
+            nums[i] = tmp[j];
+        }
     };
 
-    return f(nums);
+    f(0, nums.size() - 1);
+    return nums;
 }
 
 int main() {
