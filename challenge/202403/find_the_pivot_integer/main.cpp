a47595ad298d842f0de416ec4eca0f5192b34bf5
Shohei YOSHIDA <syohex@gmail.com>
2024-03-13 15:59:27 +0900

    Add find the pivot integer

---
+++ challenge/202403/find_the_pivot_integer/main.cpp
@@ -0,0 +1,22 @@
+#include <cassert>
+
+int pivotInteger(int n) {
+    int total = (1 + n) * n / 2;
+    int sum = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+        if (sum == total - sum + i) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    assert(pivotInteger(8) == 6);
+    assert(pivotInteger(1) == 1);
+    assert(pivotInteger(4) == -1);
+    return 0;
+}
