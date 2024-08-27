096f935f30d4c83b9953a57550642a45cfdca0dc
Shohei YOSHIDA <syohex@gmail.com>
2024-08-27 11:21:06 +0900

    Add path with maximum probability but TLE

---
+++ challenge/202408/path_with_maximum_probability/main.cpp
@@ -0,0 +1,56 @@
+#include <cassert>
+#include <vector>
+#include <map>
+
+double maxProbability(int n, const std::vector<std::vector<int>> &edges, const std::vector<double> &succProb, int start_node,
+                      int end_node) {
+    std::map<int, std::vector<std::pair<int, double>>> graph;
+
+    for (std::size_t i = 0; i < edges.size(); ++i) {
+        const auto edge = edges[i];
+        double score = succProb[i];
+        graph[edge[0]].push_back({edge[1], score});
+        graph[edge[1]].push_back({edge[0], score});
+    }
+
+    std::vector<double> max_scores(n, 0);
+    std::vector<std::pair<int, double>> q;
+    q.push_back({start_node, 1.0});
+
+    while (!q.empty()) {
+        const auto [node, current_score] = q.back();
+        q.pop_back();
+
+        for (const auto &[next, score] : graph[node]) {
+            double next_score = current_score * score;
+            if (next_score > max_scores[next]) {
+                max_scores[next] = next_score;
+                q.push_back({next, next_score});
+            }
+        }
+    }
+
+    return max_scores[end_node];
+}
+
+int main() {
+    {
+        std::vector<std::vector<int>> edges{{0, 1}, {1, 2}, {0, 2}};
+        std::vector<double> succProb{0.5, 0.5, 0.2};
+        double ret = maxProbability(3, edges, succProb, 0, 2);
+        assert(ret == 0.25);
+    }
+    {
+        std::vector<std::vector<int>> edges{{0, 1}, {1, 2}, {0, 2}};
+        std::vector<double> succProb{0.5, 0.5, 0.3};
+        double ret = maxProbability(3, edges, succProb, 0, 2);
+        assert(ret == 0.3);
+    }
+    {
+        std::vector<std::vector<int>> edges{{0, 1}};
+        std::vector<double> succProb{0.5};
+        double ret = maxProbability(3, edges, succProb, 0, 2);
+        assert(ret == 0.0);
+    }
+    return 0;
+}
